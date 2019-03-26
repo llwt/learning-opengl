26f7b6136695546692e54e366abd8203b42437a9
Steven Nance <stevenmnance@gmail.com>
2019-03-25 22:49:43 -0700

    end of video 3 (configure glew)

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -7,7 +9,6 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
-
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -19,6 +20,15 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	GLenum glewErr = glewInit();
+	if (GLEW_OK != glewErr)
+	{
+	  fprintf(stderr, "GLEW init error: %s\n", glewGetErrorString(glewErr));
+	  return -1;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
