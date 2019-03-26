e303aa11090de2273b8b799a2c4caf87d221eb22
Steven Nance <stevenmnance@gmail.com>
2019-03-25 22:07:34 -0700

    end of video 2

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -8,6 +8,7 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -24,6 +25,12 @@ int main(void)
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.0f,  0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
