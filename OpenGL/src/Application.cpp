5c54ccfc2a075d10c4d81e31810efa0aa65def21
Steven Nance <stevenmnance@gmail.com>
2019-04-07 22:55:37 -0700

    video 9 - square with index buffers

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -120,10 +120,16 @@ int main(void)
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	
-	float positions[6] = {
+	float positions[] = {
 		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.5f,
+		 0.5f,  0.5f,
+		-0.5f,  0.5f,
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	unsigned int buffer;
@@ -135,6 +141,11 @@ int main(void)
 	glVertexAttribPointer(POSITION_ATTRIB, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 	glEnableVertexAttribArray(POSITION_ATTRIB);
 
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), &indices, GL_STATIC_DRAW);
+
 	ShaderProgramSource source = ReadShaderSource("Basic");
 	unsigned int shader = CreateShader(source);
 	glUseProgram(shader);
@@ -145,7 +156,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
